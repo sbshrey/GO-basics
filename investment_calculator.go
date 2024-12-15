bf9c7cd72c9df2e32cd8e2cc0f459f24ac33ceb9
Shrey Baheti <shrey_baheti@cargill.com>
2024-12-16 05:18:36 +0530

    add decimal to inferred data type automatically

--- investment_calculator.go
+++ investment_calculator.go
@@ -7,8 +7,7 @@ import (
 
 func main() {
 	// camelCase
-	var investmentAmount, years float64 = 1000, 10
-	expectedReturnRate := 5.5
+	investmentAmount, years, expectedReturnRate := 1000.0, 10.0, 5.5
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	fmt.Println(futureValue)
