aa2383cb5674c9ff6e3023252a4fb7de344e2abc
Shrey Baheti <shrey_baheti@cargill.com>
2024-12-16 05:15:28 +0530

    inferred data type

--- investment_calculator.go
+++ investment_calculator.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	// camelCase
 	var investmentAmount float64 = 1000
-	var expectedReturnRate = 5.5
+	expectedReturnRate := 5.5
 	var years float64 = 10
 
 	var futureValue = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
