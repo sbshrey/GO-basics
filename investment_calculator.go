47008f76e8ceb1c2acda7126ec5ba085c39417b6
Shrey Baheti <sbshrey@gmail.com>
2024-12-16 05:22:47 +0530

    inflation adjusted real value

--- investment_calculator.go
+++ investment_calculator.go
@@ -7,8 +7,14 @@ import (
 
 func main() {
 	// camelCase
-	investmentAmount, years, expectedReturnRate := 1000.0, 10.0, 5.5
+	const inflationRate = 2.5
+	var investmentAmount float64 = 1000
+	years := 10.0
+	expectedReturnRate := 5.5
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 	fmt.Println(futureValue)
+	fmt.Println((futureRealValue))
 }
