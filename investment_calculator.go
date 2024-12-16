0507b34f9001ec4a2684a82c57f3f6af421320a8
Shrey Baheti <sbshrey@gmail.com>
2024-12-16 05:34:35 +0530

    final code for investment calculator

--- investment_calculator.go
+++ investment_calculator.go
@@ -23,6 +23,6 @@ func main() {
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
-	fmt.Println(futureValue)
-	fmt.Println((futureRealValue))
+	fmt.Println("Future Value: ", futureValue)
+	fmt.Println("Future Real Value: ", futureRealValue)
 }
