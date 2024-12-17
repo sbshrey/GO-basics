eb09bfbd8a765922fa04c89d5f0a887636873b3f
Shrey Baheti <sbshrey@gmail.com>
2024-12-17 08:56:24 +0530

    simplified implementaiton using funcs

--- profit_calculator.go
+++ profit_calculator.go
@@ -5,51 +5,33 @@ import (
 )
 
 func main() {
-	// camelCase
-	const inflationRate = 2.5
-	var revenue, expenses, taxRate float64 // must have a type if not given default value
-
-	// fetching data from terminal (scan)
-	fmt.Print("Revenue: ")
-	fmt.Scan(&revenue) // need to pass pointer to work
-
-	fmt.Print("Expenses: ")
-	fmt.Scan(&expenses)
-
-	fmt.Print("Tax Rate: ")
-	fmt.Scan(&taxRate)
-
-	// earningsBeforeTax := revenue - expenses
-
-	// profit := earningsBeforeTax * (1 - taxRate/100)
-
-	// ratio := earningsBeforeTax / profit
+	revenue := inputText("Revenue: ")
+	expenses := inputText("Expenses: ")
+	taxRate := inputText("Tax Rate: ")
 
 	ebt, profit, ratio := calculateValues(revenue, expenses, taxRate)
-
-	// formattedEBT := fmt.Sprintf("EBT: %.2f\n", earningsBeforeTax)
-	// fmt.Print(formattedEBT)
-	// fmt.Println("Profit: ", profit)
-	// fmt.Println("Ratio: ", ratio)
 	outputText(ebt, profit, ratio)
+}
 
+func inputText(inputText string) float64 {
+	var userInput float64
+	fmt.Print(inputText)
+	fmt.Scan(&userInput)
+	return userInput
 }
 
 func calculateValues(revenue float64, expenses float64, taxRate float64) (ebt float64, profit float64, ratio float64) {
 	ebt = revenue - expenses
-
 	profit = ebt * (1 - taxRate/100)
-
 	ratio = ebt / profit
 
 	return ebt, profit, ratio // personally preferred to know what i am returning
 	// return
-
 }
 
 func outputText(earningsBeforeTax float64, profit float64, ratio float64) {
 	formattedEBT := fmt.Sprintf("EBT: %.2f\n", earningsBeforeTax)
 	fmt.Print(formattedEBT)
 	fmt.Println("Profit: ", profit)
-	fmt.Println("Ratio: ", ratio)
+	fmt.Printf("Ratio: %.2f\n", ratio)
 }
