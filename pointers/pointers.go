74f6764065d506b6823b01b21fab3f6b26eb8564
Shrey Baheti <shrey_baheti@cargill.com>
2024-12-30 09:31:41 +0530

    simplified code

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -3,10 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	age := 32 // regular variable
-	agePointer := &age
-	fmt.Println("Age:", *agePointer) // de-referencing
-	editAgetoAdultYears(agePointer)
+	age := 32                // regular variable
+	fmt.Println("Age:", age) // de-referencing
+	editAgetoAdultYears(&age)
 	fmt.Println(age)
 
 }
