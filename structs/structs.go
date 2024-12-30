10df6f9966d3444479502c52117305be54faeeed
Shrey Baheti <shrey_baheti@cargill.com>
2024-12-30 10:19:39 +0530

    exposing user struct from separate module

--- structs/structs.go
+++ structs/structs.go
@@ -1,17 +1,10 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"time"
-)
 
-type user struct {
-	firstName string
-	lastName  string
-	birthdate string
-	createdAt time.Time
-}
+	"example.com/structs/user"
+)
 
 func main() {
 
@@ -19,9 +12,9 @@ func main() {
 	userLastName := getUserData("Please enter your last name: ")
 	userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
 
-	// var appUser user
+	// var appUser *user.User
 	// appUser = user{} // null value instance
-	appUser, err := newUser(userFirstName, userLastName, userBirthdate) // Keep a note if you applied the values in correct order
+	appUser, err := user.NewUser(userFirstName, userLastName, userBirthdate) // Keep a note if you applied the values in correct order
 
 	if err != nil {
 		fmt.Println(err)
@@ -30,32 +23,10 @@ func main() {
 
 	// ... do something awesome with gathered data!
 
-	appUser.outputUserDetails()
-	appUser.clearUserName()
-	appUser.outputUserDetails()
+	appUser.OutputUserDetails()
+	appUser.ClearUserName()
+	appUser.OutputUserDetails()
 
-}
-
-func newUser(firstName, lastName, birthdate string) (*user, error) {
-	if firstName == "" || lastName == "" || birthdate == "" {
-		return nil, errors.New("first name, last name, birthdate are required")
-	}
-
-	return &user{
-		firstName,
-		lastName,
-		birthdate,
-		time.Now(),
-	}, nil
-}
-
-func (u *user) outputUserDetails() {
-	fmt.Println(u.firstName, u.lastName, u.birthdate)
-}
-
-func (u *user) clearUserName() {
-	u.firstName = ""
-	u.lastName = ""
 }
 
 func getUserData(promptText string) string {
---
+++ structs/user/user.go
@@ -0,0 +1,36 @@
+package user
+
+import (
+	"errors"
+	"fmt"
+	"time"
+)
+
+type User struct {
+	firstName string
+	lastName  string
+	birthdate string
+	createdAt time.Time
+}
+
+func NewUser(firstName, lastName, birthdate string) (*User, error) {
+	if firstName == "" || lastName == "" || birthdate == "" {
+		return nil, errors.New("first name, last name, birthdate are required")
+	}
+
+	return &User{
+		firstName,
+		lastName,
+		birthdate,
+		time.Now(),
+	}, nil
+}
+
+func (u *User) OutputUserDetails() {
+	fmt.Println(u.firstName, u.lastName, u.birthdate)
+}
+
+func (u *User) ClearUserName() {
+	u.firstName = ""
+	u.lastName = ""
+}
