82254c21f77bfa43ede219a7c033b111d1de44cd
Shrey Baheti <shrey_baheti@cargill.com>
2024-12-30 09:30:43 +0530

    proper naming to avoid confusion

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -6,11 +6,11 @@ func main() {
 	age := 32 // regular variable
 	agePointer := &age
 	fmt.Println("Age:", *agePointer) // de-referencing
-	getAdultYears(agePointer)
+	editAgetoAdultYears(agePointer)
 	fmt.Println(age)
 
 }
 
-func getAdultYears(ageP *int) {
+func editAgetoAdultYears(ageP *int) {
 	*ageP = *ageP - 18
 }
