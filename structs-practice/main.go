1c23d681b5cf6bc7926ecb620e659db9a78d6cfc
Shrey Baheti <shrey_baheti@cargill.com>
2024-12-30 17:52:25 +0530

    switch use in any value func

--- structs-practice/main.go
+++ structs-practice/main.go
@@ -57,6 +57,7 @@ func main() {
 		fmt.Println(err)
 		return
 	}
+	printSomething(todo)
 
 	err = outputData(note)
 	if err != nil {
@@ -66,7 +67,14 @@ func main() {
 }
 
 func printSomething(value interface{}) {
-	fmt.Println(value)
+	switch value.(type) {
+	case int:
+		fmt.Println("Integer:", value)
+	case float64:
+		fmt.Println("Float:", value)
+	case string:
+		fmt.Println(value)
+	}
 }
 
 func outputData(data outputtable) error {
