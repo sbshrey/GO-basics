20257c579e4e6e8c72ea242a7dec88f342c81d7c
Shrey Baheti <sbshrey@gmail.com>
2024-12-30 18:21:35 +0530

    another way of reading types and handling values

--- structs-practice/main.go
+++ structs-practice/main.go
@@ -67,14 +67,29 @@ func main() {
 }
 
 func printSomething(value interface{}) {
-	switch value.(type) {
-	case int:
-		fmt.Println("Integer:", value)
-	case float64:
-		fmt.Println("Float:", value)
-	case string:
-		fmt.Println(value)
+	intVal, ok := value.(int) // ok is boolean
+	if ok {
+		fmt.Println("Integer:", intVal)
+		return
 	}
+	floatVal, ok := value.(float64) // ok is boolean
+	if ok {
+		fmt.Println("Float:", floatVal)
+		return
+	}
+	stringVal, ok := value.(string) // ok is boolean
+	if ok {
+		fmt.Println("String:", stringVal)
+		return
+	}
+	// switch value.(type) {
+	// case int:
+	// 	fmt.Println("Integer:", value)
+	// case float64:
+	// 	fmt.Println("Float:", value)
+	// case string:
+	// 	fmt.Println(value)
+	// }
 }
 
 func outputData(data outputtable) error {
