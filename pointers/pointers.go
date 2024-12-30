006bdfc6fbf20b0bbc753505a94cd51dbf1e7fa1
Shrey Baheti <shrey_baheti@cargill.com>
2024-12-30 09:29:36 +0530

    without return, mutate via pointers

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -6,11 +6,11 @@ func main() {
 	age := 32 // regular variable
 	agePointer := &age
 	fmt.Println("Age:", *agePointer) // de-referencing
-	adultYears := getAdultYears(agePointer)
-	fmt.Println(adultYears)
+	getAdultYears(agePointer)
+	fmt.Println(age)
 
 }
 
-func getAdultYears(ageP *int) int {
-	return *ageP - 18
+func getAdultYears(ageP *int) {
+	*ageP = *ageP - 18
 }
