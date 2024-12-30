fc3ca9369bb7b15bc9914b6bf10cb8488129436a
Shrey Baheti <shrey_baheti@cargill.com>
2024-12-30 09:27:30 +0530

    using pointers to avoiding unnecessary data copies

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -4,11 +4,13 @@ import "fmt"
 
 func main() {
 	age := 32 // regular variable
-	fmt.Println("Age:", age)
-	fmt.Println(getAdultYears(age))
+	agePointer := &age
+	fmt.Println("Age:", *agePointer) // de-referencing
+	adultYears := getAdultYears(agePointer)
+	fmt.Println(adultYears)
 
 }
 
-func getAdultYears(age int) int {
-	return age - 18
+func getAdultYears(ageP *int) int {
+	return *ageP - 18
 }
